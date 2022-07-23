5e7ffc3c1ef2a67e8f80017d4d6e32bedf80485d
Alexander Gvozdev <agvozdev.dev@gmail.com>
2022-07-23 16:35:22 +0700

    аддаптив страницы с партнерами

--- src/components/shared/PostsItems/PostsItems.tsx
+++ src/components/shared/PostsItems/PostsItems.tsx
@@ -4,40 +4,40 @@ import React from 'react'
 import './styles.scss'
 
 const PostsItems = () => {
-    const nodes = useStaticQuery(query).allMdx.nodes
-    return (
-        <ul className="posts__list">
-            {nodes.map((node: any, index: number) => {
-                const to = `/blog/${node.slug}`
-                return (
-                    <li className="posts__item" key={`posts__item-${index}`}>
-                        <Link className="posts__link" to={to}>
-                            <GatsbyImage
-                                className="posts__image"
-                                image={getImage(node.frontmatter.image)}
-                                alt={node.frontmatter.image_alt}
-                            />
-                        </Link>
+  const nodes = useStaticQuery(query).allMdx.nodes
+  return (
+    <ul className="posts__list">
+      {nodes.map((node: any, index: number) => {
+        const to = `/blog/${node.slug}`
+        return (
+          <li className="posts__item" key={`posts__item-${index}`}>
+            <Link className="posts__link" to={to}>
+              <GatsbyImage
+                className="posts__image"
+                image={getImage(node.frontmatter.image)}
+                alt={node.frontmatter.image_alt}
+              />
+            </Link>
 
-                        <div className="posts__content">
-                            <div className="posts__content-wrapper">
-                                <Link className="posts__link posts__title-link" to={to}>
-                                    <p className="posts__title">{node.frontmatter.title}</p>
-                                </Link>
+            <div className="posts__content">
+              <div className="posts__content-wrapper">
+                <Link className="posts__link posts__title-link" to={to}>
+                  <p className="posts__title">{node.frontmatter.title}</p>
+                </Link>
 
-                                <p className="posts__introduction">
-                                    {node.frontmatter.introduction}
-                                </p>
-                            </div>
-                            <Link className="posts__link posts__link-read" to={to}>
-                                Читать
-                            </Link>
-                        </div>
-                    </li>
-                )
-            })}
-        </ul>
-    )
+                <p className="posts__introduction">
+                  {node.frontmatter.introduction}
+                </p>
+              </div>
+              <Link className="posts__link posts__link-read" to={to}>
+                Читать
+              </Link>
+            </div>
+          </li>
+        )
+      })}
+    </ul>
+  )
 }
 
 export const query = graphql`
--- src/pages/partners.tsx
+++ src/pages/partners.tsx
@@ -9,34 +9,38 @@ import {GatsbyImage, getImage} from "gatsby-plugin-image";
 
 // @ts-ignore
 const PartnersPage = () => {
-    const edges = useStaticQuery(query).allFile.edges
+  const edges = useStaticQuery(query).allFile.edges
 
-    return (
-        <Layout pageTitle="Блог">
-            <section className="partners partners__bg">
-                <PageTitle text="Партнеры" />
-                <Container>
+  return (
+    <Layout pageTitle="Блог">
+      <section className="partners partners__bg">
+        <PageTitle text="Партнеры"/>
+        <p className="partners__text">
+          Наша компания работает с представленными брендами.
+          На все товары предоставляется гарантия. Цены от производителя.
+        </p>
+        <div className="partners__logos">
+          <Container>
+            <ul className="partners__list">
+              {edges.map((edge: any) => {
+                return (
+                  <li className="partners__item">
+                    <GatsbyImage alt='Логотип партнера' image={getImage(edge.node)}/>
+                  </li>
+                )
+              })}
 
-                    <ul className="partners__list">
-                            {edges.map((edge: any) => {
-                                return (
-                                    <li className="partners__item">
-                                        <GatsbyImage alt='Логотип партнера' image={getImage(edge.node)} />
-                                    </li>
-                                )
-                            })}
-
-                    </ul>
-                </Container>
-
-                <div className="partners__wrapper">
-                    <Container>
-                        <Contacts extraClass={'partners__contacts'} />
-                    </Container>
-                </div>
-            </section>
-        </Layout>
-    )
+            </ul>
+          </Container>
+        </div>
+        <div className="partners__wrapper">
+          <Container>
+            <Contacts extraClass={'partners__contacts'}/>
+          </Container>
+        </div>
+      </section>
+    </Layout>
+  )
 }
 
 export const query = graphql`
