3a504890e178dcca90037e55958a330ba44fe512
Alexander Gvozdev <agvozdev.dev@gmail.com>
2022-08-07 03:12:58 +0700

    добавлены meta tags

--- src/components/shared/Layout/Layout.tsx
+++ src/components/shared/Layout/Layout.tsx
@@ -1,6 +1,6 @@
 import Header from 'components/shared/Header'
 import React from 'react'
-import { useStaticQuery, graphql } from 'gatsby'
+import {useStaticQuery, graphql} from 'gatsby'
 import {Helmet} from "react-helmet";
 import Footer from 'components/shared/Footer'
 
@@ -9,27 +9,33 @@ type LayoutProps = {
   children: React.ReactNode
 }
 
-const Layout: React.FC<LayoutProps> = ({ pageTitle, children }) => {
+const Layout: React.FC<LayoutProps> = ({pageTitle, children}) => {
   const data = useStaticQuery(graphql`
     {
       site {
         id
         siteMetadata {
           title
+          keywords
+          description
         }
       }
     }
   `)
 
   const title = `${pageTitle} | ${data.site.siteMetadata.title}`
+
   return (
     <>
       <Helmet>
         <title>{title}</title>
+        <meta name="description" content={`${data.site.siteMetadata.description}`} />
+        <meta name="robots" content="all" />
+        <meta name="keywords" content={data.site.siteMetadata.keywords} />
       </Helmet>
-      <Header />
+      <Header/>
       <main>{children}</main>
-      <Footer />
+      <Footer/>
     </>
   )
 }
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import Layout from "../components/shared/Layout";
+import MainScreen from "../components/main-page/ServiceSlider";
+import AboutUs from "../components/main-page/about-us/AboutUs";
+import PartnerSlider from "../components/shared/PartnerSlider";
+import Prices from "../components/main-page/Prices";
+import Posts from "../components/main-page/Posts";
+import Contacts from "../components/shared/Contacts";
 
-const NotFound = () => {
-  return <>нет такой страницы</>
-}
+const NotFound = () => (
+  <Layout pageTitle="404 Страница не существует">
+      404 Страница не существует
+  </Layout>
+)
 
 export default NotFound
